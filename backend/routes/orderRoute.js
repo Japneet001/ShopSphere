0c7553bbdaddfb513f4b020588e4af55ea7d99ac
Japneet Kaur <121337325+Japneet001@users.noreply.github.com>
2024-12-09 19:47:13 +0530

    Backend Features

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,27 @@
+import express from "express";
+import adminAuth from "../middlewares/adminAuth.js";
+import {
+    allOrders,
+    placeOrderCOD,
+    placeOrderRazorpay,
+    placeOrderStripe,
+    updateStatus,
+    userOrders,
+} from "../controllers/orderController.js";
+import authUser from "../middlewares/auth.js";
+
+const orderRouter = express.Router();
+
+// Admin Features
+orderRouter.post("/list", adminAuth, allOrders);
+orderRouter.post("/status", adminAuth, updateStatus);
+
+// Payment Features
+orderRouter.post("/place", authUser, placeOrderCOD);
+orderRouter.post("/stripe", authUser, placeOrderStripe);
+orderRouter.post("/razorpay", authUser, placeOrderRazorpay);
+
+// User Features
+orderRouter.post("/userorders", authUser, userOrders);
+
+export default orderRouter;
