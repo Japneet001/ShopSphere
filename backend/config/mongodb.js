ac626e856f6c348bfc9f600a85080cc505a0a9ad
Japneet Kaur <121337325+Japneet001@users.noreply.github.com>
2024-12-09 19:41:21 +0530

    ShopSphere

--- backend/config/mongodb.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const connectDB = async () => {
-    mongoose.connection.on("connected", () => {
-        console.log("DB Connected");
-    });
-    await mongoose.connect(`${process.env.MONGODB_URI}/ShopSphere`);
-};
-
-export default connectDB;
