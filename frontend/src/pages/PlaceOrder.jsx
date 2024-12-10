cfdef96d453aef2bc94850e87873010a7640a132
Japneet Kaur <121337325+Japneet001@users.noreply.github.com>
2024-12-10 12:21:21 +0530

    Update PlaceOrder.jsx

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -79,8 +79,9 @@ const PlaceOrder = () => {
                 case "stripe":
                     const responseStripe = await axios.post(backendUrl+'/api/order/stripe',orderData,{headers:{token}});
                     if(responseStripe.data.success){
-                        // const{session_url} = responseStripe.data
-                        // window.location.replace(session_url)
+                        const{session_url} = responseStripe.data
+                        if(session_url) window.location.replace(session_url);
+                        else toast.error(responseStripe.data.message);
                         setCartItems({});
                         navigate("/orders");
                     }
